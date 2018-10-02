22777dbdc48df4a8f67168afdb7660d93735d9e8
rafsal <rafsalkrahim@gmail.com>
2018-10-02 09:53:03 +0530

    basic looping

--- node_kb/app.js
+++ node_kb/app.js
@@ -8,11 +8,32 @@ app.set('view engine','pug');
 
 //Home Route
 app.get ('/',function(req,res){
+    let articles=[
+        {
+            id:1,
+            title:"artic one",
+            author:"Base 1",
+            body:"1:Its fun to code in node using pug"
+        },
+        {
+            id:2,
+            title:"artic two",
+            author:"Base 2",
+            body:"2:Its fun to code in node using pug"
+        }
+    
+    ];
     res.render('index',{
-        title:'test 23'
+        title:'test 23',
+        articles:articles
     });
 });  
 
+app.get('/article/add',function(req,res){
+res.render('add_article',{
+title:"Add Article "
+});
+});
 
 //Start Server
 app.listen(3000,function(){
